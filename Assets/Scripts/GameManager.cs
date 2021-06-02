3a584c8bbe9e63ebe38f7faca64209a7dc8a2088
Gian Franco Catania <catania.gian@gmail.com>
2021-06-02 00:37:18 -0300

    He is in charge of controlling the game.
    Create a composition with the BoardManager class to call its Setup() method.
    
    It has a Singleton design pattern to create an instance of it in a static way to call it from any Script.
    
    It contains a special method so that it always exists in the scene if it is deleted.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+[RequireComponent(typeof(BoardManager))]
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager _instanceGameManager;    
+    private BoardManager _boardManager;
+
+    public static GameManager InstanceGameManager { get => _instanceGameManager; set => _instanceGameManager = value; }
+
+    private void Awake()
+    {
+        if (_instanceGameManager == null)
+            _instanceGameManager = this;
+        else if (_instanceGameManager != this)
+            Destroy(this.gameObject);
+
+        DontDestroyOnLoad(this.gameObject);
+
+        _boardManager = GetComponent<BoardManager>();
+    }
+
+    private void Start()
+    {
+        _boardManager.Setup();        
+    }
+}
