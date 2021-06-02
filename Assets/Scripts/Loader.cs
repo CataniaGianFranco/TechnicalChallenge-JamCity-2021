e06bb60aac88a1ac83cdc8dc03d670c7f49797a4
Gian Franco Catania <catania.gian@gmail.com>
2021-06-02 00:40:50 -0300

    Load a prefab.
    Script to instantiate the GameManager.

---
+++ Assets/Scripts/Loader.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class Loader : MonoBehaviour
+{
+    public GameObject gameManager;
+
+    private void Awake()
+    {
+        if (GameManager.InstanceGameManager == null)
+            Instantiate<GameObject>(gameManager);
+    }
+}
