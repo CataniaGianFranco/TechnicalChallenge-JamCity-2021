1421c105bb177485fb32f3f54c2c39ffd5ddfe47
Gian Franco Catania <catania.gian@gmail.com>
2021-06-04 11:58:58 -0300

    Mouse detection test and Mesh Collider.
       A Mesh Collider component was added to each pre-made hexagon.
       As it is a simple model, using this component will not cause an over-performance problem. If it is necessary to use this component as in this case, a Mesh of the same model with low polygons is implemented so as not to lose visual quality.

---
+++ Assets/Scripts/ClickableTile.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickableTile : MonoBehaviour
+{
+    public void OnMouseUp()
+    {
+        Debug.Log("Click this tile");
+    }
+}
