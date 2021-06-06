3a5cd7142b64f2720862155eef0b2ff76a56f24c
Gian Franco Catania <catania.gian@gmail.com>
2021-06-06 10:44:54 -0300

    Script for hexagons

---
+++ Assets/Scripts/NodePosition.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public class NodePosition : MonoBehaviour
+{
+    public Vector2 _nodeCurrentPosition;
+    public Vector2 NodeCurrentPosition { get => _nodeCurrentPosition; set => _nodeCurrentPosition = value; }
+}
