8c0222b9451527612a1e10f6e93924a638347af9
Gian Franco Catania <catania.gian@gmail.com>
2021-06-08 00:45:49 -0300

    Update GameManager.cs

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     /* This class contains Singleton */
+    private GameObject _initialHexagon, _targetHexagon;
     private static GameManager _instanceGameManager;    
     private BoardManager _boardManager;
 
     private int _countClick = 2;
     public int CountClick { get => _countClick; set => _countClick = value; }
+    public GameObject InitialHexagon { get => _initialHexagon; set => _initialHexagon = value; }
+    public GameObject TargetHexagon { get => _targetHexagon; set => _targetHexagon = value; }
     public static GameManager InstanceGameManager { get => _instanceGameManager; set => _instanceGameManager = value; }
 
     private void Awake()
