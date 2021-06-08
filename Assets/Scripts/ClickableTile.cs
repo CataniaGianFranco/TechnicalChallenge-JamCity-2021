225e8939a98bdba2c03a8725e1264a6135149f68
Gian Franco Catania <catania.gian@gmail.com>
2021-06-08 00:23:26 -0300

    Update ClickableTile.cs
    Contains a condition to check the number of clicks remaining to start performing algorithm A*.

--- Assets/Scripts/ClickableTile.cs
+++ Assets/Scripts/ClickableTile.cs
@@ -2,14 +2,20 @@ using UnityEngine;
 
 public class ClickableTile : MonoBehaviour
 {
+    private GameManager _scriptGameManager;
     private MeshRenderer _thisMeshRenderer;
-    public void Awake()
+    public void Start()
     {
         _thisMeshRenderer = GetComponent<MeshRenderer>();
+        _scriptGameManager = GameObject.Find("GameManager(Clone)").GetComponent<GameManager>();
+        
     }
     public void OnMouseUp()
     {
+        if (_scriptGameManager.CountClick != 0 && _thisMeshRenderer.material.color != Color.green)
+        {
             _thisMeshRenderer.material.color = Color.green;
-
+            _scriptGameManager.CountClick--;
+        }
     }
 }
