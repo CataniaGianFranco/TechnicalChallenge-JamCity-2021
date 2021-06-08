be257953940d8d59f8260c4aeea69b9d9eb66650
Gian Franco Catania <catania.gian@gmail.com>
2021-06-08 10:58:17 -0300

    Update NodePosition.cs

--- Assets/Scripts/NodePosition.cs
+++ Assets/Scripts/NodePosition.cs
@@ -1,7 +1,22 @@
+using System.Collections.Generic;
 using UnityEngine;
+using PathFinding;
 
-public class NodePosition : MonoBehaviour
+public class NodePosition : MonoBehaviour, IAStarNode
 {
     public Vector2 _nodeCurrentPosition;
     public Vector2 NodeCurrentPosition { get => _nodeCurrentPosition; set => _nodeCurrentPosition = value; }
+    public IEnumerable<IAStarNode> Neighbours => throw new System.NotImplementedException();
+
+    IEnumerable<IAStarNode> IAStarNode.Neighbours => throw new System.NotImplementedException();
+
+    public float CostTo(IAStarNode neighbour)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public float EstimatedCostTo(IAStarNode target)
+    {
+        throw new System.NotImplementedException();
+    }
 }
